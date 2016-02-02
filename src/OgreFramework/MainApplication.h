cd8ca483ecb973fc46b9c27303e1bebf32823ea6
Valentin Roucoux <val.roucoux@free.fr>
2016-02-02 11:31:53 +0100

    add animation camera

--- src/OgreFramework/MainApplication.h
+++ src/OgreFramework/MainApplication.h
@@ -138,6 +138,7 @@ namespace OgreFramework
 		KeyboardState & m_keyboardState ;
 		///< The camera manager
 		RTSCameraManager * m_cameraManager ;
+		Ogre::AnimationState * mCamAnimState;
 
 		//Ogre::SceneNode * m_shipShape ;
 		//GameElements::NullAgent::Pointer m_entityAdapter ;
